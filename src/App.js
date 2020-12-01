25aa313928376e5642bb12fdb4bcd54e5633b3e4
Edson Reginold <edson@Edsons-MacBook-Pro.local>
2020-12-01 20:22:55 +0530

    client component routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import React, {Component} from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { Component } from 'react'
 import Login from './Components/loginComponent/login'
 import Register from './Components/registerComponent/register'
 import Main from './Components/MainComponent/main'
@@ -14,58 +14,67 @@ import './App.css';
 import AddExercise from './Components/AddorUpdateExerciseComponent/AddExercise'
 import User from './Components/UserComponent/user'
 import Error404 from './Components/404pageComponent/Error404'
+import Client from './Components/clientComponent/Client'
 
-export default class App extends Component{
+export default class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      isLoggedin:false
+      isLoggedin: false,
+      role: ''
     }
     this.onLoggingIn = this.onLoggingIn.bind(this)
     this.onLogOut = this.onLogOut.bind(this)
   }
-  componentDidMount(){
-    if(localStorage.getItem('token')){
-      this.setState({isLoggedin: true})
-    }else{
-      this.setState({isLoggedin: false})
+  componentDidMount() {
+    if (localStorage.getItem('token')) {
+      const role = localStorage.getItem('role')
+      if(role){
+        this.setState({role})
+      }
+      this.setState({ isLoggedin: true })
+    } else {
+      this.setState({ isLoggedin: false })
     }
   }
-  
-  onLoggingIn(){
-    this.setState({isLoggedin: true})
+
+  onLoggingIn() {
+    const role = localStorage.getItem('role')
+    this.setState({ isLoggedin: true, role})
   }
-  onLogOut(){
+  onLogOut() {
     localStorage.removeItem('token')
-    this.setState({isLoggedin: false})
+    localStorage.removeItem('role')
+
+    this.setState({ isLoggedin: false, role: '' })
   }
-  render(){
-  return (
-    <div className = 'App'>
-    <Router>
-        <Navbar isLoggedin = {this.state.isLoggedin} onLogOut = {() => { this.onLogOut()}}/>
-      <Switch>
-        <Route path = '/login'  render = {(props) => <Login {...props} onLoggingIn = {this.onLoggingIn} />}/>
-        <Route path = '/register'  render = {(props) => <Register {...props} onLoggingIn = {this.onLoggingIn}/>}/>
-        <ProtectedRoute exact path = '/exercise' component = {Exercise}/>
-        <ProtectedRoute exact path = '/equipment' component = {Equipment}/>
-        <Route path = '/equipment/add' render = {(props) => <ProtectedRoute {...props} component = {AddEquipment}/>} />
-        <Route path = '/equipment/update/:id' render = {(props) => <ProtectedRoute {...props} component = {AddEquipment}/>} />
-        <Route path = '/exercise/update/:id' render = {(props) => <ProtectedRoute {...props} component = {AddExercise}/>} />
-        <Route path = '/exercise/add' render = {(props) => <ProtectedRoute {...props} component = {AddExercise}/>} />
-        <Route path = '/customer' render = {(props) => <ProtectedRoute {...props} component = {User}/>} />
-        <ProtectedRoute exact path = '/' component = {Main}/>
-        <Route component = {Error404}/>
-
-
-        {/* <ProtectedRoute  path = '/exercise/add' component = {AddExercise}/>
-        <ProtectedRoute  path = '/exercise/update/:id' component = {AddExercise}/> */}
-      </Switch>
-    </Router>
-    </div>
+  render() {
     
-  );
-}
+    return (
+      <div className='App'>
+        <Router>
+          <Navbar isLoggedin={this.state.isLoggedin} onLogOut={() => { this.onLogOut() }} />
+
+          <Switch>
+            <Route path='/login' render={(props) => <Login {...props} onLoggingIn={this.onLoggingIn} />} />
+            <Route path='/register' render={(props) => <Register {...props} onLoggingIn={this.onLoggingIn} />} />
+            <ProtectedRoute exact path='/exercise' component={Exercise} />
+            <ProtectedRoute exact path='/equipment' component={Equipment} />
+            <Route path='/equipment/add' render={(props) => <ProtectedRoute {...props} component={AddEquipment} />} />
+            <Route path='/equipment/update/:id' render={(props) => <ProtectedRoute {...props} component={AddEquipment} />} />
+            <Route path='/exercise/update/:id' render={(props) => <ProtectedRoute {...props} component={AddExercise} />} />
+            <Route path='/exercise/add' render={(props) => <ProtectedRoute {...props} component={AddExercise} />} />
+            <Route path='/customer' render={(props) => <ProtectedRoute {...props} component={User} />} />
+            <ProtectedRoute exact path='/' component={Main} />
+            <Route exact path ='/client' render = { (props) => <ProtectedRoute {...props}  component = {Client}/>} />
+            <Route component={Error404} />
+          </Switch>
+
+        </Router>
+      </div>
+
+    );
+  }
 }
 
